23b984c8a9871fb2f80100fad4d717938f64f778
Aniruddha Biswas Atanu <aniruddhabiswas105@gmail.com>
2024-11-02 04:24:39 -0500

    Splash added

---
+++ app/src/main/java/com/example/greenpulse/SplashActivity.java
@@ -0,0 +1,38 @@
+package com.example.greenpulse;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.WindowManager;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.greenpulse.databinding.ActivitySplashBinding;
+
+public class SplashActivity extends AppCompatActivity {
+
+    ActivitySplashBinding binding;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivitySplashBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        Thread td = new Thread(){
+            public void run(){
+                try{
+                    sleep(3000);
+                }catch(Exception e)
+                {
+                    e.printStackTrace();
+                }
+                finally {
+                    Intent intent = new Intent(SplashActivity.this,MainActivity.class);
+                    startActivity(intent);
+                    finish();
+                }
+            }
+        };td.start();
+    }
+}
